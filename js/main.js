a2268f39243f78bd688d56c0beea3664690617b2
ferjpi <fernandojpt1203@gmail.com>
2018-09-15 05:39:40 -0500

    subiendo los archivos para cargar la pagina

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+  });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+const lent = document.querySelector('#lente')
+const buscador = document.querySelector('#mobile-search')
+lent.addEventListener('click', function() {
+  buscador.style.display = 'block'
+  lent.style.display = 'none'
+})
+
+const close = document.querySelector('#close')
+close.addEventListener('click', function() {
+  buscador.style.display = 'none'
+  lent.style.display = 'block'
+})
